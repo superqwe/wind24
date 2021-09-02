92cc957cea6481e2e9b0484c495aa382c6950aa3
superqwe <superqwe@users.noreply.github.com>
2021-09-02 21:38:01 +0200

    prove

--- wind24.py
+++ wind24.py
@@ -6,8 +6,19 @@ import numpy as np
 # import matplotlib.pyplot as plt
 from unicodedata import normalize
 
+N2_KM_H = 1.852
+
 FIN = r'D:\Studio\Python\wind24\html\210301a.htm'
 table_MN = pd.read_html(FIN)
 df = table_MN[0]
-df['V.Media'].replace({' nodi':''}, regex=True, inplace=True)
-print(df)
+df['V.Media'].replace(' nodi','', regex=True, inplace=True)
+
+df = df[:-2]
+
+df.astype({'V.Media': 'float'})
+
+print (df.dtypes)
+
+##df['v'] = float(df['V.Media'])
+##
+##print(df)
