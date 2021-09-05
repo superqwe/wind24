a5c3893c129572338c262eb9d86e72b8f50c4e56
superqwe <superqwe@users.noreply.github.com>
2021-09-05 05:58:44 +0200

    salva sql analizza mese

--- mareografico.py
+++ mareografico.py
@@ -8,6 +8,7 @@ from pprint import pprint as pp
 
 import numpy as np
 import pandas as pd
+import sqlite3 as db
 
 ANNO = 2021
 MESE = 3
@@ -50,6 +51,57 @@ def gradi2settore(df):
     return df
 
 
+def salva_sqlite(df):
+    con = db.connect('ispra.sqlite')
+    df.to_sql('ISPRA', con, if_exists='replace')
+
+
+def analizza_mese(df, anno=ANNO, mese=MESE):
+    data_inizio = datetime.date(anno, mese, 1)
+    data_fine = datetime.date(anno, mese, calendar.monthrange(anno, mese)[1])
+    date_range = pd.date_range(data_inizio, data_fine)
+
+    dati = []
+    for giorno in date_range:
+        data = int(giorno.strftime('%Y%m%d'))
+        df1 = df[df['data'] == data]
+
+        # velocità media
+        v = df1['vel'].mean()
+
+        if False:
+            pass
+        # if np.isnan(v) or v < 5.0:
+        #     direzione = 'V'
+        else:
+            # direzione dominante
+            print(df1.groupby('dir').count())
+            df2 = df1.drop(df1[df1['dir'] == 'C'].index)
+
+            try:
+                direzione = df2.dir.mode().values[0]
+            except IndexError:
+                print('***', 'tutto il giorno calma di vento')
+                direzione = 'Variabile'
+
+            print(data, '---', direzione, '\n' * 3)
+
+        #
+        dati.append([giorno.strftime('%d/%m/%Y'), v, direzione])
+
+    # pp(dati)
+    return dati
+
+
+def scrivi_dati(dati):
+    righi = ['%s\t%.1f\t%s' % (data, vel, settore) for data, vel, settore in dati]
+    tabella = '\n'.join(righi)
+    print(tabella)
+
+
 if __name__ == '__main__':
     dati = leggi_dati()
     dati = gradi2settore(dati)
+    salva_sqlite(dati)
+    dati = analizza_mese(dati)
+    scrivi_dati(dati)
